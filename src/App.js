a103d68b4e867f8ad9b90cd9f112f141a3f03203
Taras Dovbyus <79213421+dstaras@users.noreply.github.com>
2021-09-15 16:24:31 +0300

    netlify

--- src/App.js
+++ src/App.js
@@ -1,35 +1,35 @@
-import { lazy, Suspense, useEffect } from 'react';
-import { Switch, Route } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
+import { lazy, Suspense, useEffect } from "react";
+import { Switch, Route } from "react-router-dom";
+import { useDispatch } from "react-redux";
 
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-import PrivateRoute from './components/PrivateRoute';
-import PublicRoute from './components/PublicRoute';
+import PrivateRoute from "./components/PrivateRoute";
+import PublicRoute from "./components/PublicRoute";
 
-import Container from './components/Container';
-import AppBar from './components/AppBar';
-import LoaderComponent from './components/LoaderComponent';
+import Container from "./components/Container";
+import AppBar from "./components/AppBar";
+import LoaderComponent from "./components/LoaderComponent";
 
-import { authOperations } from './redux/auth';
+import { authOperations } from "./redux/auth";
 
 const HomeView = lazy(() =>
-  import('./views/HomeView' /* webpackChunkName: "home-view" */),
+  import("./views/HomeView" /* webpackChunkName: "home-view" */)
 );
 
 const RegisterView = lazy(() =>
-  import('./views/RegisterView' /* webpackChunkName: "register-view" */),
+  import("./views/RegisterView" /* webpackChunkName: "register-view" */)
 );
 const LoginView = lazy(() =>
-  import('./views/LoginView' /* webpackChunkName: "login-view" */),
+  import("./views/LoginView" /* webpackChunkName: "login-view" */)
 );
 const ContactsView = lazy(() =>
-  import('./views/ContactsView' /* webpackChunkName: "contacts-view" */),
+  import("./views/ContactsView" /* webpackChunkName: "contacts-view" */)
 );
 
 const NotFoundView = lazy(() =>
-  import('./views/NotFoundView' /* webpackChunkName: "not-found-view" */),
+  import("./views/NotFoundView" /* webpackChunkName: "not-found-view" */)
 );
 
 function App() {
